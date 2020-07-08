3c1fb303e4fe19c910b551fa7044ae88746d5262
Sam Marston <sammarston15@gmail.com>
2020-07-07 19:13:02 -0600

    got the cards put together to display the info

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CharacterItem from './CharacterItem';
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (<h1>loading...</h1>) : (<section className='cards'>
+    {items.map(item => (
+      <CharacterItem key={item.char_id} item={item}/>
+    ))}
+  </section>)
+};
+
+export default CharacterGrid;
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,18 +1,9 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import logo from '../../logo.png';
-import axios from 'axios';
+
 
 const Header = () => {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
-      console.log(result.data)
-    }
-    fetchItems();
-  }, [])
+  
 
   return (
     <div className='center'>
