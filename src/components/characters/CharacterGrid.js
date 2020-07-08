ce9ed73e719c140ee03b876d240d207c83781e24
Sam Marston <sammarston15@gmail.com>
2020-07-07 19:37:01 -0600

    got search working and added spinner on load

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,8 +1,8 @@
 import React from "react";
 import CharacterItem from './CharacterItem';
-
+import Spinner from '../ui/Spinner'
 const CharacterGrid = ({ items, isLoading }) => {
-  return isLoading ? (<h1>loading...</h1>) : (<section className='cards'>
+  return isLoading ? (<Spinner />) : (<section className='cards'>
     {items.map(item => (
       <CharacterItem key={item.char_id} item={item}/>
     ))}
